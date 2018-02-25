d181be1a540f2e1589de07e3e0778dde41e923c4
Herc <32612354+culaspiro@users.noreply.github.com>
2018-02-25 22:17:53 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,45 @@
+#***************************# 
+#Exploratory Data Analysis  #
+#Course Project 1: Plot 2   #
+#***************************#
+
+if(!file.exists("./data")){
+  dir.create("./data")
+}
+
+#Download the data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <-"./data/exdata_data_household_power_consumption.zip"
+download.file(fileUrl,destfile)
+
+#Unzip file and save
+outDir <- "./EDA"
+zipfile <- "./data/exdata_data_household_power_consumption.zip"
+unzip(zipfile, exdir=outDir)
+
+#Load the data
+datafile <- "./EDA/household_power_consumption.txt"
+data <- read.table(datafile, header = TRUE, sep = ";", na.strings = "?")
+
+#dim(data)
+#[1] 2075259       9
+#head(data)
+#str(data)
+
+#Convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+#Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+#Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+data$datetime <- as.POSIXct(data$datetime)
+
+#Create and save plot 2
+attach(data)
+plot(Global_active_power ~ datetime, type = "l",ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
+dev.off()
+detach(data)
